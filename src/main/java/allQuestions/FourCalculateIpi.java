00b81f61dc94401da09c1ac0f1dee3274c2940b4
diegoholiveirav <diegoholiveirav@gmail.com>
2021-04-13 11:00:25 -0300

    Change question 4

--- src/main/java/allQuestions/FourCalculateIpi.java
+++ src/main/java/allQuestions/FourCalculateIpi.java
@@ -4,35 +4,39 @@ import java.util.Scanner;
 
 public class FourCalculateIpi {
 	Scanner sc = new Scanner(System.in);
-	float percent; int id; float value; int amount;
 	
-	public float constructor(float a, int b, float c, int d, int e, float f, int g) {
-		System.out.print("Porcentagem % do IPI a ser acescido no valor das peças: ");
-		this.percent = sc.nextFloat();
+	public float percent; 
+	public int id; 
+	public float value; 
+	public int amount;
+	public float finalValue;
+	
+	public float getPercent(float ipi) {
+		return this.percent = ipi;
+	}
+	
+	public int getId(int productId) {
+		return this.id = productId;
+	}
+	
+	public float getValue(float productValue) {
+		return this.value = productValue;
+	}
+	
+	public int getAmount(int productAmount) {
+		return this.amount = productAmount;
+	}
+	
+	public float setFinalValue(
+			float finalValueOne,
+			float finalValueTwo
+		) 
+	{
+		this.finalValue = 
+		(finalValueOne) 
+		+ (finalValueTwo) 
+		/ (percent/100 + 1);
 		
-		System.out.print("Código da peça 01: ");
-		this.id = sc.nextInt();
-		
-		System.out.print("Valor da peça 01: ");
-		this.value = sc.nextFloat();
-		
-		System.out.print("Quantidade de peças: ");
-		this.amount = sc.nextInt();
-		
-		float stepOneForumla = (this.value * this.amount);
-		
-		//About second product		
-		System.out.print("Código da peça 02: ");
-		this.id = sc.nextInt();
-		
-		System.out.print("Valor da peça 02: ");
-		this.value = sc.nextFloat();
-		
-		System.out.print("Quantidade de peças: ");
-		this.amount = sc.nextInt();
-		
-		float stepTwoFormula = (this.value * this.amount);
-		
-		return (stepOneForumla + stepTwoFormula) * (this.percent/100 + 1);
+		return this.finalValue;
 	}
 }
--- src/test/java/allQuestions/TestFourCalculateIpi.java
+++ src/test/java/allQuestions/TestFourCalculateIpi.java
@@ -1,19 +1,64 @@
 package allQuestions;
 
+import java.util.Scanner;
+
 import org.junit.Test;
 
 import junit.framework.Assert;
 
 @SuppressWarnings("deprecation")
 public class TestFourCalculateIpi {
-	FourCalculateIpi calculateIpi = new FourCalculateIpi();
+	Scanner sc = new Scanner(System.in);
 	
-	@SuppressWarnings("deprecation")
+	float value;
+	int amount;
+	float percent;
+	float finalValue;
+	float finalValueOne;
+	float finalValueTwo;
+	
+	FourCalculateIpi productOne = new FourCalculateIpi();
+	FourCalculateIpi productTwo = new FourCalculateIpi();
+	
+	
 	@Test
 	public void TestFourCalculatorIpi() {
-		Assert.assertEquals(220.0, 
-				calculateIpi.constructor(10, 1, 100, 1, 2, 100, 1), 
-				220.0);
+		System.out.print("IPI: ");
+		percent = productOne.getPercent(sc.nextFloat());
+		
+		System.out.println("Primeiro produto");
+		
+		System.out.print("Código do produto: ");
+		productOne.getId(sc.nextInt());
+		
+		System.out.print("Valor: ");
+		value = productOne.getValue(sc.nextFloat());
+		
+		System.out.print("Quantidade: ");
+		amount = productOne.getAmount(sc.nextInt());
+		
+		finalValueOne = value * amount;
+		
+		System.out.println("Segundo produto");
+		
+		System.out.print("Código do produto: ");
+		productTwo.getId(sc.nextInt());
+		
+		System.out.print("Valor: ");
+		value = productTwo.getValue(sc.nextFloat());
+		
+		System.out.print("Quantidade: ");
+		amount = productTwo.getAmount(sc.nextInt());
+		
+		finalValueTwo = value * amount;
+		
+		finalValue = productOne.setFinalValue(finalValueOne, finalValueTwo);
+		
+		Assert.assertEquals(
+				finalValue, 
+				productOne.setFinalValue(finalValueOne, finalValueTwo),
+				finalValue
+		);
 	}
 
 }
